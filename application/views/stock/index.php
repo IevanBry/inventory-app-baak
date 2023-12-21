8280d9a98da288a3c9a375e8a1ceceef55c01af7
ievan <ievan22ti@mahasiswa.pcr.ac.id>
2023-12-21 11:26:00 +0700

    menambahkan

--- application/views/stock/index.php
+++ application/views/stock/index.php
@@ -169,7 +169,7 @@
                                     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                     <div class="relative p-4 w-full max-w-2xl max-h-full">
                                         <!-- Modal content -->
-                                        <div class="relative bg-white rounded shadow">
+                                        <div class="relative bg-white rounded shadow pb-1">
                                             <!-- Modal header -->
                                             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
                                                 <h3 class="text-lg font-semibold text-gray-90">
@@ -177,7 +177,7 @@
                                                 </h3>
                                                 <button type="button"
                                                     class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                                                    data-modal-toggle="updateBarangModal">
+                                                    data-modal-toggle="updateBarangModal<?= $no ?>">
                                                     <svg class="w-3 h-3" aria-hidden="true"
                                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                                         <path stroke="currentColor" stroke-linecap="round"
@@ -267,7 +267,7 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="text-end">
+                                                <div class="text-end me-4 mb-4">
                                                     <button type="button" data-modal-hide="updateBarangModal<?= $no ?>"
                                                         class="text-end bg-white shadow-md hover:bg-gray-100 border text-gray-500 font-medium rounded text-sm px-3 py-2">
                                                         Batal
