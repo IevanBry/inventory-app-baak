382a213792d98f14746a642ee0a6288ca89214df
rdhohdyat <rdho.hdyat@gmail.com>
2024-01-05 21:22:15 +0700

    masukan barang ke keranjang

--- application/views/stock/index.php
+++ application/views/stock/index.php
@@ -170,7 +170,7 @@
                                 </tr>
                                 <!-- update barang modal -->
                                 <div id="updateBarangModal<?= $no ?>" tabindex="-1" aria-hidden="true"
-                                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ">
                                     <div class="relative p-4 w-full max-w-2xl max-h-full">
                                         <!-- Modal content -->
                                         <div class="relative bg-white rounded shadow pb-1">
@@ -466,8 +466,9 @@
                                         </svg>
                                         <p class="mb-2 text-sm text-gray-500">Upload Gambar Barang</p>
                                     </div>
-                                    <input name="gambar" id="dropzone-file" type="file" class="hidden" />
+                                   
                                 </label>
+                                 <input name="gambar" id="dropzone-file" type="file"  />
                             </div>
                         </div>
                     </div>
