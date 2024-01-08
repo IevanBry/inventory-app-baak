968acd781cd05c0d505a850e03d38300527b892b
rdhohdyat <rdho.hdyat@gmail.com>
2024-01-08 16:11:11 +0700

    history table

--- application/views/dashboard/history.php
+++ application/views/dashboard/history.php
@@ -1,4 +1,4 @@
-<div class="w-full ml-12 sm:ml-0 p-3 bg-white ">
+<div class="w-full ml-12 sm:ml-0 p-3">
     <div class="w-full  mb-3  p-2 rounded">
         <div class="items-center justify-between block sm:flex ">
             <div class="flex items-center mb-4 sm:mb-0">
@@ -14,32 +14,67 @@
             </div>
         </div>
     </div>
-    <div class="grid grid-cols-1 gap-2 p-2">
+    <div class=" rounded border shadow-md ml-2">
+        <div class="flex flex-col">
+            <div class="overflow-x-auto">
+                <div class="inline-block min-w-full align-middle">
+                    <div class="overflow-hidden shadow">
+                        <table class="min-w-full divide-y divide-gray-200 table-fixed">
+                            <thead class="bg-gray-100">
+                                <tr>
+                                    <th scope="col" class="p-4 text-xs font-medium text-left  uppercase">
+                                       No
+                                    </th>
+                                    <th scope="col" class="p-4 text-xs font-medium text-left  uppercase">
+                                        User
+                                    </th>
+                                    <th scope="col" class="p-4 text-xs font-medium text-left  uppercase">
+                                        Tanggal
+                                    </th>
+                                    <th scope="col" class="p-4 text-xs font-medium text-left  uppercase">
+                                        Barang
+                                    </th>
+                                    <th scope="col" class="p-4 text-xs font-medium text-left  uppercase">
+                                        Jumlah
+                                    </th>
+                                    <th scope="col" class="p-4 text-xs font-medium text-left  uppercase">
+                                        Status
+                                    </th>
 
-        <?php for ($i = 0; $i < 10; $i++) { ?>
-            <div class="notif-card bg-white shadow-md p-4 rounded border max-w-[80%]">
-                <div class="flex text-sm justify-between items-center">
-                    <div class="flex gap-4 items-center">
-                        <?php if ($i % 2 == 0) { ?>
-                            <div class="bg-green-100 p-1 flex items-center rounded">
-                                <i class='bx bx-check-circle text-green-500'></i>
-                            </div>
-                        <?php } else { ?>
-                            <div class="bg-red-100 p-1 flex items-center rounded">
-                                <i class='bx bx-x-circle text-red-500'></i>
-                            </div>
-                        <?php } ?>
-                        <div>
-                            <h1 class="text-base font-medium">Permintaan barang : Permintaan 4 buah spidol</h1>
-                            <div class="flex items-center gap-2 text-sm text-gray-500">
-                                <i class='bx bx-calendar '></i>
-                                <span>20 November 2023</span>
-                            </div>
-                        </div>
+
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white " id="tableBody">
+                                <tr class="border-b ">
+                                    <td class="p-2 text-sm font-medium pl-6 text-gray-900">
+                                      1
+                                    </td>
+                                    <td class="p-2 text-sm font-medium text-gray-900">
+                                        Ridho Hidayat
+                                    </td>
+                                    <td class="p-2 text-sm font-medium text-gray-900">
+                                       2 Januari 2024
+                                    </td>
+                                    <td class="p-2 text-sm pl-4 font-medium text-gray-900">
+                                      Sendok
+                                    </td>
+                                    <td class="p-2 pl-10 text-sm font-medium text-gray-900">
+                                        9
+                                    </td>
+
+                                    <td class="p-2 text-sm font-medium text-gray-900">
+                                        <span class="bg-amber-300 text-white shadow-md p-1 px-3 rounded">
+                                            proses
+                                        </span>
+                                    </td>
+                                </tr>
+                              
+                            </tbody>
+                        </table>
                     </div>
-                    <i class='bx bx-dots-vertical-rounded' ></i>
                 </div>
             </div>
-        <?php } ?>
+        </div>
     </div>
+</div>
 </div>
